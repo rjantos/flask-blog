f6722591d6c26cb85b4d4485c3a36dcd950ec8e0
Rudolf Jantos <rudolf.jantos@gmail.com>
2016-07-02 10:24:36 +0200

    added basic views/routes/templates

--- blog.py
+++ blog.py
@@ -1,7 +1,8 @@
 # blog.py - controller
-
-#imports
+# imports
 from flask import Flask, render_template, request, session, flash, redirect, url_for, g
+import sqlite3
+
 
 # config
 DATABASE = 'blog.db'
@@ -12,8 +13,21 @@ app = Flask(__name__)
 app.config.from_object(__name__)
 
 # function used fro connecting to the database
+
+
 def connect_db():
     return sqlite3.connect(app.config['DATABASE'])
+
+
+@app.route('/')
+def login():
+    return render_template('login.html')
+
+
+@app.route('/main')
+def main():
+    return render_template('main.hmtl')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
